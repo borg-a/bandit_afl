8ee255e36e606419ccb4774b31d9e9dfcd3cb2eb
aborgare <aborgare>
2022-06-07 18:55:24 +0200

    lr 1.0 , decay 1.0

--- src/afl-fuzz.c
+++ src/afl-fuzz.c
@@ -2142,8 +2142,8 @@ int main(int argc, char **argv_orig, char **envp) {
   afl->ucb_confidence_value = 0.1;
 
   // exp3
-  afl->exp3_learning_rate = 0.1;
-  afl->exp3_decay_value = 0.99999;
+  afl->exp3_learning_rate = 1.0;
+  afl->exp3_decay_value = 1.0;
   afl->exp3_max_update = 10;
   afl->exp3_max_weight = 10;
 
